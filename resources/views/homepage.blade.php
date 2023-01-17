c7e328eb467678d4bd138401aa8de6dea5653a54
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2023-01-17 10:22:29 +0100

    Corregge array notation in stampa dinamica cards

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -14,11 +14,11 @@
                 <div class="col-2">
                     <div class="card" style="width: 18rem;">
                         <div class="card-body">
-                            <h5 class="card-title">{{$movie["title"]}}</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">{{$movie["original_title"]}}</h6>
-                            <p class="card-text">{{$movie["nationality"]}}</p>
-                            <p class="card-text">{{$movie["date"]}}</p>
-                            <strong class="card-text">{{$movie["vote"]}}</strong>
+                            <h5 class="card-title">{{ $movie->title }}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">{{ $movie->original_title }}</h6>
+                            <p class="card-text">{{ $movie->nationality }}</p>
+                            <p class="card-text">{{ $movie->date }}</p>
+                            <strong class="card-text">{{ $movie->vote }}</strong>
                         </div>
                     </div>
                 </div>
